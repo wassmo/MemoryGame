2dc66f1842e090dd9321b19a3c4047cf402a8f5d
Olga Wasilewska <oswasilewska@gmail.com>
2018-03-13 16:43:52 +0100

    Default game reset and classList progress

--- js/app.js
+++ js/app.js
@@ -1,25 +1,32 @@
 
 //cards
 
-let card = document.getElementsByClassName("card");
+let card = document.querySelectorAll(".card");
 let cards = [...card];
-console.log(cards);
 
-for (var i = 0; i < cards.length; i++){
-    cards[i].addEventListener("click", showCard);
-};
+//deck of cards
 
-//function changing class open and show to disabled
+const deck = document.querySelector(".deck");
 
-var showCard = ()=>{
-    this.classList.toggle("open");
-    this.classList.toggle("show");
-    this.classList.toggle("disabled");
-}
+//matching card class items
+
+let matchedCard = document.querySelectorAll(".match");
+
+//opened card class items
+
+let openedCards = [];
+
+
+//score-moves
+
+let moves = 0;
+let counter = document.querySelector(".moves");
+
+
 
 //shuffling cards (Fisher–Yates method)
 
-var shuffle = array => {
+function shuffle (array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -32,17 +39,111 @@ var shuffle = array => {
     return array;
 }
 
-//changing position of cards, starting game
+//After page loading, the game is starting
+document.onload = startGame();
 
-const deck = document.querySelector(".deck");
+
+// starting game
 
 
-var startGame=()=>{
-    var shuffledCards = shuffle(cards);
-    for (var i= 0; i < shuffledCards.length; i++){
-        [].forEach.call(shuffledCards, function(item){
+
+function startGame(){
+
+    //shuffling cards
+    cards = shuffle(cards);
+
+    //changing position of shuffled cards and removing already existing classes
+    for (var i= 0; i < cards.length; i++){
+
+        deck.innerHTML = "";
+        [].forEach.call(cards, function(item){
             deck.appendChild(item);
         });
+        cards[i].classList.remove('open', 'disabled', 'match', 'remove');
     }
+
+
+    //function changing class open and show to disabled
+
+    function showCard() {
+        this.classList.toggle("open");
+        this.classList.toggle("show");
+        this.classList.toggle("disabled");
+    }
+
+
+    //-------function deciding what is happening after two cards showing
+    function cardOpen() {
+        openedCards.push(this);
+        console.log(openedCards);
+
+
+        var lengthOfOpened = openedCards.length;
+        if(lengthOfOpened === 2){
+            moveCounter();
+            if(openedCards[0].type === openedCards[1].type){
+                matched();
+            } else {
+                unmatched();
+            }
+        }
+    };
+
+    //if cards match
+
+    function matched(){
+        openedCards[0].classList.add("match");
+        openedCards[1].classList.add("match");
+        openedCards[0].classList.remove("show", "open");
+        openedCards[1].classList.remove("show", "open");
+        openedCards = [];
+    }
+
+
+    //if card don't match
+
+    function unmatched(){
+        openedCards[0].classList.add("unmatched");
+        openedCards[1].classList.add("unmatched");
+        disable();
+        setTimeout(function(){
+            openedCards[0].classList.remove("show", "open", "unmatched");
+            openedCards[1].classList.remove("show", "open", "unmatched");
+            enable();
+            openedCards = [];
+        },1100);
+    }
+
+    //disabling cards (this cause that player can use only two cards at once)
+
+    function disable(){
+        Array.prototype.filter.call(cards, function(card){
+            card.classList.add('disabled');
+        });
+    }
+
+    //enabling cards (this cause that player can use only two cards at once)
+
+
+    function enable(){
+        Array.prototype.filter.call(cards, function(card){
+            card.classList.remove('disabled');
+            for(var i = 0; i < matchedCard.length; i++){
+                matchedCard[i].classList.add("disabled");
+            }
+        });
+    }
+
+//--------------------end of part---------------
+
+//Loop which is adding afterclick function to each card
+    for (var i = 0; i < cards.length; i++){
+        cards[i].addEventListener("click", showCard);
+        cards[i].addEventListener("click", cardOpen);
+    };
+
+
+
 }
+
 
